c7ebb20d667c24576bc0bef9d90e1819f43ffd53
prajan410 <prajangit@gmail.com>
2026-03-10 11:06:18 -0600

    User edit/delete profile

--- SyntaxAppProjectFolder/app/build.gradle.kts
+++ SyntaxAppProjectFolder/app/build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     implementation(libs.core.ktx)
     implementation(libs.firebase.auth)
     implementation(libs.firebase.firestore)
+    implementation(libs.cardview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
