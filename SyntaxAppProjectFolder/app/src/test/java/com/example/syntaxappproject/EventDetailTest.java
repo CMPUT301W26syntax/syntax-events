65a1d5bd4202bbaddda13191020e7a3643b2d6c8
willi97407 <wli14@ualberta.ca>
2026-03-13 01:46:12 -0600

    update javadoc of EventDetail and some new of EventDetailTest

--- SyntaxAppProjectFolder/app/src/test/java/com/example/syntaxappproject/EventDetailTest.java
+++ SyntaxAppProjectFolder/app/src/test/java/com/example/syntaxappproject/EventDetailTest.java
@@ -2,6 +2,10 @@ package com.example.syntaxappproject;
 
 
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
 import org.junit.Test;
 /**
  * Unit tests for the {@link EventDetail} model class.
@@ -11,9 +15,53 @@ import org.junit.Test;
  * </p>
  */
 public class EventDetailTest {
-    @Test
-    public void testAdd() {
+    private EventDetail event;
 
+    /**
+     * Initializes a event before test:
+     */
+    @Before
+    public void setUp() {
+        event = new EventDetail(
+                "event1",
+                "Test Event",
+                "Test Description",
+                "Test Location",
+                100,
+                true,
+                "2026-04-01",
+                "2026-04-02",
+                "2026-03-01",
+                "2026-03-30",
+                10,
+                "Random",
+                "poster.jpg"
+        );
+    }
+    /**
+     * Verifies that the constructor initializes event detail correctly.
+     */
+    @Test
+    public void testConstructor() {
+        assertEquals("event1", event.getEventId());
+        assertEquals("Test Event", event.getName());
+        assertEquals("Test Description", event.getDescription());
+        assertEquals("Test Location", event.getLocation());
+        assertEquals(100, event.getCapacity());
+        assertTrue(event.isGeoReq());
+        assertEquals("2026-04-01", event.getStartingEventDate());
+        assertEquals("2026-04-02", event.getEndingEventDate());
+        assertEquals("2026-03-01", event.getStartingRegistrationPeriod());
+        assertEquals("2026-03-30", event.getEndingRegistrationPeriod());
+        assertEquals(10, event.getWaitlistCount());
+        assertEquals("Random", event.getLotteryCriteria());
+        assertEquals("poster.jpg", event.getPoster());
+    }
+    /**
+     * Verifies that the no-argument constructor initializes event detail correctly.
+     */
+    @Test
+    public void testNoArgConstructor() {
 
     }
 }
